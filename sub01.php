b5813485880c0db4b5037a3cbfdcbe8c3b2da4b2
LeeYongSeok13 <minamiharuka333@gmail.com>
2022-08-11 15:45:33 +0900

    20220811

---
+++ sub01.php
@@ -0,0 +1,30 @@
+<?
+$subnum = 1;
+$pagenum = 1;
+$sub_title = 'ceo 인사말'
+?>
+<? include 'head.php' ?>
+<div class="content">
+    <p>Lorem ipsum dolor sit amet.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, iusto?</p>
+    <p>Lorem ipsum dolor sit.</p>
+</div>
+
+
+<table class="table01">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>구분</th>
+            <th>가격</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>오뎅</td>
+            <td>1억원</td>
+        </tr>
+    </tbody>
+</table>
+<? include 'tail.php' ?>
---
+++ sub02.php
@@ -0,0 +1,7 @@
+<?
+$subnum = 2;
+$sub_title = '솔루션'
+?>
+<? include 'head.php' ?>
+나는 서버페이지0<?= $subnum ?>
+<? include 'tail.php' ?>
